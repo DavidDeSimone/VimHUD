913e9a5b80eb2a2b6f3e8d2eff0db5c43f6deef3
David DeSimone <djd231@scarletmail.rutgers.edu>
2014-10-12 10:52:03 -0400

    Updated Command mode detection

--- plugin/modeslist.py
+++ plugin/modeslist.py
@@ -76,7 +76,7 @@ class ModesList:
             print char
 
             #If we see the marker for command mode
-            if char == '^[' and mode == 'insert':
+            if char == 3 and mode == 'insert':
                 #Set the mode to command mode
                 mode = 'command'
 
