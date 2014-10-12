d419d14def2e91f51ba0212d931153acadb21054
brugel18@gmail.com <brugel18@gmail.com>
2014-10-12 09:08:21 -0400

    asdf

--- plugin/recommender.py
+++ plugin/recommender.py
@@ -4,6 +4,7 @@ import random
 
 usefullness_file_name= "usefullness.txt"
 user_stats_file_name = "user_stats.txt"
+user_regexs_file_name = "user_regexs_file_name.txt"
 command_description_file_name = "command.txt"
 command_description_insert_file_name = "command_description_insert.txt"
 
@@ -11,9 +12,11 @@ command_description_insert_file_name = "command_description_insert.txt"
 def load_stats():
     usefulness = dict()
     user_stats = dict()
+    regexs = dict()
     load_usefull(usefulness)
     load_user_stats(user_stats)
-    return usefulness, user_stats
+    load_regex_stats(regexs)
+    return usefulness, user_stats, regexs
 
 def load_description():
     description = dict()
@@ -23,6 +26,12 @@ def load_description():
         description[line[0]] = line[1]
     return description
 
+
+def load_regex_stats(stats):
+    stats_file = open(user_regexs_file_name, 'r')
+    for line in stats_file:
+        line = string.split(line, '\t')
+        stats[line[0]] = int(line[1])
 
 
 def load_usefull(usefulness):
@@ -38,12 +47,12 @@ def load_user_stats(user_stats):
         user_stats[line[0]] = math.exp(-int(line[1])/200.0)#learn constant??
 
 
-def combine_features(usefullness, user_stats, recent_stats):
+def combine_features(usefullness, user_stats, recent_stats, regexs):
     #assert(len(usefullness) == len(user_stats) and len(user_stats) == len(recent_stats))
     try:
         probabilities = list()
         for key in usefullness.keys():
-            probabilities.append((30.0*usefullness[key]+recent_stats[key])*recent_stats[key])
+            probabilities.append((30.0*usefullness[key]+regexs[key])*recent_stats[key])
 
         #create a cdf for the (unweighted) probabilities
         for i in xrange(1, len(probabilities)):
@@ -55,24 +64,30 @@ def combine_features(usefullness, user_stats, recent_stats):
         for i in xrange(1, len(probabilities)-1):
             if probabilities[i-1] < rv and probabilities[i] >= rv:
                 return usefullness.keys()[i]
+        if usefullness.keys()[len(probabilities)-1][1] == "UNBOUNDED":
+            combine_features(usefullness, user_stats, recent_stats, regexs)
         return usefullness.keys()[len(probabilities)-1]
     except:
         return usefullness.keys()[0]#not really handling need to do something
 
 
 def test():
-    usefullness, user_stats = load_stats()
-    desc = load_description()
-    recent_stats = usefullness
-    random =  combine_features(usefullness, user_stats, recent_stats)
-    return str(random) + ": " + str(desc[random])
+    try:
+        usefullness, user_stats, regexs = load_stats()
+        desc = load_description()
+        recent_stats = usefullness
+        random =  combine_features(usefullness, user_stats, recent_stats, regexs)
+        return str(random) + ": " + str(desc[random])
+    except:
+        return test()
+
 
 
 def main():
-    usefullness, user_stats = load_stats()
+    usefullness, user_stats, regexs = load_stats()
     desc = load_description()
     recent_stats = usefullness
-    random =  combine_features(usefullness, user_stats, recent_stats)
+    random =  combine_features(usefullness, user_stats, recent_stats, regexs)
     print str(random) + ": " + str(desc[random])
 
 if __name__ == "__main__":
