7c2736e155ed405a91b2a7e2844dc93ac8c2b743
brugel18@gmail.com <brugel18@gmail.com>
2015-01-19 18:49:20 -0500

    install path

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 #Install Script for VimHud
 
 echo "alias vim='vim -w ~/.vimlog \"\$@\"'" >> ~/.bashrc
-export PYTHONPATH=$PYTHONPATH:~/.vim/bundle/VimParser/plugin
+export PYTHONPATH=$PYTHONPATH:~/.vim/bundle/VimHUD/plugin
